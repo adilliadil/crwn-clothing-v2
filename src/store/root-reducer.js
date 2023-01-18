21b711b7b0fd50aca1e1853605009d51941bae13
Adil Adilli <adiladilli@google.com>
2023-01-17 21:36:03 -0800

    utilize redux thunks

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,5 +1,4 @@
 import { combineReducers } from 'redux';
-
 import { userReducer } from './user/user.reducer';
 import { categoriesReducer } from './categories/categories.reducer';
 import { cartReducer } from './cart/cart.reducer'
